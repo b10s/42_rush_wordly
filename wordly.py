1ebfd9dfe736a73aa6afc7e6a285ba1e2cac1c12
kkido <kkido@student.42tokyo.jp>
2026-03-15 18:16:23 +0900

    Error handling for dict

--- wordly.py
+++ wordly.py
@@ -3,11 +3,15 @@ import random
 from colorama import Fore, Style
 
 
-def count_lines(filepath):
+def count_lines_and_find_error(filepath):
     line_count = 0
     with open(filepath, 'r') as file:
         for line in file:
-            line_count += 1
+            word = line.strip()
+            if len(word) != 5:
+                return 0
+            else:
+                line_count += 1
     return line_count
 
 
@@ -21,24 +25,29 @@ def choose_word(line_cnt):
     return "dummy"
 
 
-print("word must be 5 letters\n")
-print("rules:")
-print(Style.DIM + " x " + Style.NORMAL + "- no letter")
-print(Fore.YELLOW + " x " + Fore.WHITE +
-      "- there is letter but different place")
-print(Fore.GREEN + " x " + Fore.WHITE + "- correct")
-print(Style.RESET_ALL, end="")
 filepath = "words.txt"
-line_cnt = count_lines(filepath)
+line_cnt = count_lines_and_find_error(filepath)
+
+if line_cnt == 0:
+    print(Fore.RED + "Error: invalid dictionaty")
+    exit(1)
+
 rnd_line = random.randint(1, line_cnt)
 word = choose_word(rnd_line)
 answer_count = 0
 
+print("\nrules:")
+print("word must be 5 letters")
+print(Style.DIM + " x " + Style.NORMAL + "- no letter")
+print(Fore.YELLOW + " x " + Fore.WHITE + "- there is letter but different place")
+print(Fore.GREEN + " x " + Fore.WHITE + "- correct")
+print(Style.RESET_ALL, end="")
+
 # print("line cnt ", line_cnt)
 # print("random line ", rnd_line)
-print("word is", word)
+# print("word is", word)
 
-while (1 == 1):
+while (1):
     print()
 
     if answer_count >= 6:
@@ -57,8 +66,7 @@ while (1 == 1):
     user_word = input("Please enter a word: ")
     print()
     if len(user_word) != 5:
-        print(Fore.RED + "Invalid input! " +
-              Fore.WHITE + "please enter 5 letters words")
+        print(Fore.RED + "Invalid input! " + Fore.WHITE + "please enter 5 letters words")
         continue
     else:
         answer_count += 1
@@ -79,6 +87,7 @@ while (1 == 1):
         else:
             print(Fore.WHITE + Style.DIM + ch, end="")
     print(Style.RESET_ALL)
+
     if user_word == word:
         print(Fore.BLUE + "\nGAME CLEAR!")
         break
